3a5506f001d957253c8fc17ee3244ce5282df5e7
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-27 11:26:23 -0300

    conteud p

--- src/components/Conteudoprincipal.tsx
+++ src/components/Conteudoprincipal.tsx
@@ -15,6 +15,11 @@ const Conteudoprincipal = (props: Props) => {
 
         <img src={imgDivConteudoEscrito} alt="" className='imgDeFundo'/>
 
+        <p className="descricaoJogo">
+          Miles Morales is Marvel's newest New York Spider-Man. After his father's untimely death, Miles was introduced
+          to Peter Parker, who quickly became his friend and mentor. When Miles was bitten by an Oscorp genetically
+          engineered spider, he developed unique powers and, after months of insistence, Peter agreed to train him.</p>
+
     </div>
 
     <div className="imgHomemArnaha">
