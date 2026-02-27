282da1a17da8d2bbf8541b20b7bc4f059dba5b70
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-27 09:50:38 -0300

    componente

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import '../src/index.css';
 
 import "../src/Css/AppDiv.css"
 
+import Header from "../src/components/Header.jsx"
+
 function App() {
   return (
     <div className="App">
 
               <div className="conteudo">
+
+                    <Header />
+
 
               </div>
 
